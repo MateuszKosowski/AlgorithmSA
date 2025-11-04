3cbca9b5e3d43603a571683ff9fd4a648f04e900
Jakub Rosiak <251620@edu.p.lodz.pl>
2025-11-04 22:10:39 +0100

    format: :rage2:

--- main.py
+++ main.py
@@ -34,45 +34,25 @@ def generate_neighbor(solution, temperature, min_x, max_x):
 
 def simulated_annealing(func, initial_temp, min_x, max_x, cooling_factor, epochs, coefficient, steps):
 
-    # Energia początkowa na chodzenie po górach
     temperature = initial_temp
-
-    # Najlepsze rozwiązanie
     best_solution = np.random.uniform(min_x, max_x)
-
-    # Liczba wykonanych kroków (lub przemyśleń nad zrobieniem kroku)
     count_of_iterations = 0
 
-    # Dopóki nasz poziom energii jest większy niż minimalny
     for _ in range(epochs):
-
-        # Generowanie pobliskich możliwych kroków
         for _ in range(steps):
-
-            # Losowy punkt w zasięgu kroku
             neighbor = generate_neighbor(best_solution, temperature, min_x, max_x)
 
-            # Różnica funkcji celu (gdzie jest wyżej)
             delta = func(neighbor) - func(best_solution)
 
-            # Jeśli jest lepiej, idziemy tam
             if delta > 0:
                 best_solution = neighbor
 
-            # Jeśli jest gorzej, idziemy tam z pewnym prawdopodobieństwem, że może być lepiej później
             else:
-
-                # Rzut kostką (element losowy)
                 x = np.random.uniform(0, 1)
-
-                # Magiczny wzór na prawdopodobieństwo
-                # Im mamy więcej sił tym chętniej zaryzykujemy
-                # Im mniejsza różnica wysokości tym chętniej zaryzykujemy
                 if x < np.exp(delta / (coefficient * temperature)):
                     best_solution = neighbor
 
             count_of_iterations += 1
-        # Raz na LiczbaIteracjiKorków tracimy trochę energii
         temperature = cooling_function(cooling_factor, temperature)
     return best_solution, count_of_iterations
 
@@ -89,11 +69,13 @@ def plot(f, min_x, max_x, amount, solution):
 
 def generate_metrics(f, epochs, temperature, alpha, steps, min_x, max_x, coefficient, max_value):
     start = time.perf_counter()
-    solution, iterations = simulated_annealing(func=f, initial_temp=temperature, min_x=min_x, max_x=max_x, cooling_factor=alpha, epochs=epochs, steps=steps, coefficient=coefficient)
+    solution, iterations = simulated_annealing(func=f, initial_temp=temperature, min_x=min_x, max_x=max_x,
+                                               cooling_factor=alpha, epochs=epochs, steps=steps,
+                                               coefficient=coefficient)
     end = time.perf_counter()
     precision = abs(f(solution) - f(max_value))
     print(f"Rozwiązanie: {solution}")
-    print(f"Czas wykonywania: {(end - start)*1000:.2f} ms")
+    print(f"Czas wykonywania: {(end - start) * 1000:.2f} ms")
     print(f"Liczba iteracji: {iterations}")
     print(f"Dokładność: {precision}\n")
     return solution
@@ -107,17 +89,20 @@ def check_f1():
     for Mi in epoch_iterations:
         for i in range(5):
             print(f"Wyniki dla ilości epok = {Mi} ({i + 1}/5)")
-            generate_metrics(f=f1, epochs=Mi, temperature=500, alpha=0.999, steps=1, min_x=-150, max_x=150, coefficient=0.1, max_value=max_task_1)
+            generate_metrics(f=f1, epochs=Mi, temperature=500, alpha=0.999, steps=1, min_x=-150, max_x=150,
+                             coefficient=0.1, max_value=max_task_1)
 
     for Ti in temperature:
         for i in range(5):
             print(f"Wyniki dla temperatury = {Ti} ({i + 1}/5)")
-            generate_metrics(f=f1, epochs=3000, temperature=Ti, alpha=0.999, steps=1, min_x=-150, max_x=150, coefficient=0.1, max_value=max_task_1)
+            generate_metrics(f=f1, epochs=3000, temperature=Ti, alpha=0.999, steps=1, min_x=-150, max_x=150,
+                             coefficient=0.1, max_value=max_task_1)
 
     for ai in alpha:
         for i in range(5):
             print(f"Wyniki dla alpha = {ai} ({i + 1}/5)")
-            generate_metrics(f=f1, epochs=3000, temperature=500, alpha=ai, steps=1, min_x=-150, max_x=150, coefficient=0.1, max_value=max_task_1)
+            generate_metrics(f=f1, epochs=3000, temperature=500, alpha=ai, steps=1, min_x=-150, max_x=150,
+                             coefficient=0.1, max_value=max_task_1)
 
 
 def check_f2():
@@ -127,29 +112,32 @@ def check_f2():
     for Mi in M:
         for i in range(5):
             print(f"Wyniki dla M = {Mi} ({i + 1}/5)")
-            generate_metrics(f=f2, epochs=Mi, temperature=5, alpha=0.997, steps=1, min_x=-1, max_x=2, coefficient=0.1, max_value=max_task_2)
+            generate_metrics(f=f2, epochs=Mi, temperature=5, alpha=0.997, steps=1, min_x=-1, max_x=2, coefficient=0.1,
+                             max_value=max_task_2)
 
     for Ti in T:
         for i in range(5):
             print(f"Wyniki dla T = {Ti} ({i + 1}/5)")
-            generate_metrics(f=f2, epochs=1200, temperature=Ti, alpha=0.997, steps=1, min_x=-1, max_x=2, coefficient=0.1, max_value=max_task_2)
+            generate_metrics(f=f2, epochs=1200, temperature=Ti, alpha=0.997, steps=1, min_x=-1, max_x=2,
+                             coefficient=0.1, max_value=max_task_2)
 
     for ai in a:
         for i in range(5):
             print(f"Wyniki dla a = {ai} ({i + 1}/5)")
-            generate_metrics(f=f2, epochs=1200, temperature=5, alpha=ai, steps=1, min_x=-1, max_x=2, coefficient=0.1, max_value=max_task_2)
+            generate_metrics(f=f2, epochs=1200, temperature=5, alpha=ai, steps=1, min_x=-1, max_x=2, coefficient=0.1,
+                             max_value=max_task_2)
 
 
 if __name__ == '__main__':
     print("Rozwiązanie domyślne funkcji 1: ")
-    s1 = generate_metrics(f=f1, epochs=3000, temperature=500, alpha=0.999, steps=1, min_x=-150, max_x=150, coefficient=0.1,
-                     max_value=max_task_1)
+    s1 = generate_metrics(f=f1, epochs=3000, temperature=500, alpha=0.999, steps=1, min_x=-150, max_x=150,
+                          coefficient=0.1, max_value=max_task_1)
 
     plot(f1, -150, 150, 500, s1)
 
     check_f1()
     print("Rozwiązanie domyślne funkcji 2: ")
     s2 = generate_metrics(f=f2, epochs=1200, temperature=5, alpha=0.997, steps=1, min_x=-1, max_x=2, coefficient=0.1,
-                     max_value=max_task_2)
+                          max_value=max_task_2)
     plot(f2, -1, 2, 1000, s2)
     check_f2()
